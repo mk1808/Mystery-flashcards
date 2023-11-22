bc11c5c390e34dd69ac66f6df2f5d7aa9915d343
Marek Czopor <marqos111222@gmail.com>
2023-11-22 22:21:12 +0100

    feature #4: add update flashcard

--- mystery-flashcards/app/api/flashcards/[id]/route.ts
+++ mystery-flashcards/app/api/flashcards/[id]/route.ts
@@ -1,4 +1,5 @@
 import FlashcardSet from "@/models/FlashcardSet";
+import { getUser } from "@/utils/server/authUtils";
 import connectToDB from "@/utils/server/database";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -11,10 +12,29 @@ export async function GET(request: NextRequest, { params }: { params: { id: stri
 
 export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
     const id = params.id;
-    console.log(id);
+    const requestBody = await request.json();
     await connectToDB();
 
-    return NextResponse.json(id);
+    const existingFlashcardSet = await FlashcardSet.findById(id);
+    const currentUser = await getUser(request);
+    const userIsAuthor = existingFlashcardSet.user._id.equals(currentUser._id)
+    if (!userIsAuthor) {
+        return new NextResponse('Access denied!', { status: 401 });
+    }
+
+    const updatedFlashCardSet = {
+        _id: existingFlashcardSet._id,
+        user: existingFlashcardSet.user,
+        name: requestBody.name,
+        level: requestBody.level,
+        hashtags: requestBody.hashtags,
+        flashcards: requestBody.flashcards,
+        isPublic: requestBody.isPublic,
+        lang1: requestBody.lang1,
+        lang2: requestBody.lang2,
+    }
+    const result = await FlashcardSet.findOneAndReplace({ _id: id }, updatedFlashCardSet, { new: true });
+    return new NextResponse(JSON.stringify(result), { status: 200 });
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
