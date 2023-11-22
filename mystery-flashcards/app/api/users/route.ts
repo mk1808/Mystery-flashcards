5f5c9b6ef746c1ba60875f795f0e0e27a3aef62e
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-11-22 21:14:35 +0100

    feature #4: get user, get user by id, put user

--- mystery-flashcards/app/api/users/route.ts
+++ mystery-flashcards/app/api/users/route.ts
@@ -1,16 +1,36 @@
 import User from "@/models/User";
+import { getUser } from "@/utils/server/authUtils";
 import connectToDB from "@/utils/server/database";
+import { hashPassword } from "@/utils/server/encryptionUtils";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     await connectToDB();
-    const user = await User.findById("655d06c50eb839e48eb1763a");
-    return NextResponse.json(user)
+    return new NextResponse(JSON.stringify(await getUser(request)));
 }
 
 export async function PUT(request: NextRequest) {
-    const body = await request.json();
     await connectToDB();
+    const userForm = await request.json();
+    const logged = await getUser(request);
+    if (userForm.password && userForm.password !== userForm.confirmPassword) {
+        return new NextResponse('Password do not match!', { status: 400 });
+    }
+    const existingUser = await User.findById(logged._id);
+    if (existingUser == null) {
+        return new NextResponse('User does not exist!', { status: 409 });
+    }
 
-    return NextResponse.json(body);
+    const updatedUser = {
+        _id: existingUser._id,
+        points: existingUser.points,
+        rang: existingUser.rang,
+        statistics: existingUser.statistics,
+        mail: userForm.mail,
+        name: userForm.name,
+        password: await hashPassword(userForm.password)
+    }
+    const result = await User.findOneAndReplace({ _id: logged._id }, updatedUser, { new: true });
+
+    return NextResponse.json(result);
 }
