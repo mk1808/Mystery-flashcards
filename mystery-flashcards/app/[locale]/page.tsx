925538e69814c13746ec3456ef329d16cfc978e1
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-04 21:25:18 +0100

    bug #41: scss fix, create playground page, clear main page

--- mystery-flashcards/app/[locale]/page.tsx
+++ mystery-flashcards/app/[locale]/page.tsx
@@ -1,8 +1,8 @@
-import RoundActionButton from "@/components/common/RoundActionButton";
+
 import FirstPanel from "@/components/main/panels/FirstPanel";
 import SecondPanel from "@/components/main/panels/SecondPanel";
 import { getDictionary } from "@/dictionaries/dictionaries"
-import { ArrowLongDownIcon } from '@heroicons/react/24/solid'
+
 import FirstSecondDivider from "@/components/main/panels/FirstSecondDivider";
 import SecondThirdDivider from "@/components/main/panels/SecondThirdDivider";
 import FlashcardSetsSearch from "@/components/main/search/FlashcardSetsSearch";
@@ -16,29 +16,11 @@ export default async function Page({ params }: { params: { locale: string } }) {
             <FirstSecondDivider />
             <SecondPanel />
             <SecondThirdDivider />
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm">
-                <div className="h-96">
-                    first
-                </div>
-                <RoundActionButton content={getButtonContent()} styles="border-[3px]" />
-                <div className="h-96" id="search-sets">
-                    second
-                    <button className="btn">Button</button>
-                    <button className="btn btn-neutral">Neutral</button>
-                    <button className="btn btn-primary">Primary</button>
-                    <button className="btn btn-secondary">Secondary</button>
-                    <button className="btn btn-accent">Accent</button>
-                    <button className="btn btn-ghost">Ghost</button>
-                    <button className="btn btn-link">Link</button>
-                    <div style={{ width: 200, height: 200 }} className="bg-primary"></div>
-                </div>
-            </div>
+           
             <FlashcardSetsSearch dictionary={dictionary} />
         </>
     )
 
-    function getButtonContent() {
-        return <ArrowLongDownIcon className="h-11 w-11" />
-    }
+
 }
 //return <div>{dictionary.common.mainPage}</div>
--- mystery-flashcards/app/layout.tsx
+++ mystery-flashcards/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
 import styles from './styles.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
