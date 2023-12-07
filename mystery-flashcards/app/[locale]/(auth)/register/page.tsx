c23eea5ac9bfbe6b3f8fe558612c800c3bc52e82
Marek Czopor <marqos111222@gmail.com>
2023-12-07 23:03:47 +0100

    feature #39: add login and register views

--- mystery-flashcards/app/[locale]/(auth)/register/page.tsx
+++ mystery-flashcards/app/[locale]/(auth)/register/page.tsx
@@ -21,11 +21,12 @@ async function Register({ locale }: { locale: string }) {
   function renderInput() {
     return (
       <div className="mt-6">
-        <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">Street address</label>
-        <div className="mt-2">
-          <input type="text" name="street-address" id="street-address" autoComplete="street-address"
-            className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-        </div>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">What is your name?</span>
+          </div>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+        </label>
       </div>
     )
   }
