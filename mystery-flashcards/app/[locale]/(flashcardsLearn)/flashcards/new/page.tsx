2ed04c98e0aa610583592d1ca3a6864617cd000f
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-08 15:26:57 +0100

    feature #38: copied layout from flashcard set details

--- mystery-flashcards/app/[locale]/(flashcardsLearn)/flashcards/new/page.tsx
+++ mystery-flashcards/app/[locale]/(flashcardsLearn)/flashcards/new/page.tsx
@@ -1,7 +1,35 @@
+import FlashcardContainer from '@/components/common/FlashcardContainer';
+import { FlashcardT } from '@/models/Flashcard';
+import { HeartIcon } from '@heroicons/react/24/outline';
 import React from 'react'
 
-export default function NewFlashcards() {
+export default function NewFlashcards({ params }: { params: { id: String } }) {
+  const singleFlashcard: FlashcardT = {
+    wordLang1: "słowo",
+    wordLang2: "word",
+    description1: "Jakiś opis słowa",
+    description2: "Some description"
+  }
+
+  const flashcards: FlashcardT[] = Array(20).fill(singleFlashcard);
   return (
-    <div>NewFlashcards</div>
+    <>
+      <div className="w-[1000px]">
+        {renderActionButtons()}
+        {flashcards.map(card => <FlashcardContainer key={card.wordLang1} card={card} />)}
+      </div>
+      <div>FlashcardsDetails {params.id}</div>
+    </>
   )
+
+  function renderActionButtons() {
+    return (
+      <div className="mb-12 flex justify-end">
+        <button className="btn btn-primary mr-10">Ucz się tej kolekcji</button>
+        <div className="tooltip tooltip-bottom" data-tip="Dodaj do ulubionych">
+          <button className="btn btn-primary"> &nbsp;<HeartIcon className="h-6 w-6" /> &nbsp; </button>
+        </div>
+      </div>
+    )
+  }
 }
