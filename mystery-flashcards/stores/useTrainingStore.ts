cbb6eb94e1cb1440abed5ec5b28657990b8920d7
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-10 21:47:28 +0100

    feature #75: training store

---
+++ mystery-flashcards/stores/useTrainingStore.ts
@@ -0,0 +1,46 @@
+import { AnswerT } from "@/models/Answer";
+import { FlashcardT } from "@/models/Flashcard";
+import { create } from "zustand";
+
+type State = {
+    flashcardSet: FlashCardSetDto,
+    allAnswers: AnswerT[],
+    roundAnswers: AnswerT[],
+    result: any,
+    allFlashcards: FlashcardT[],
+    roundFlashcards: FlashcardT[],
+    currentFlashcardIndexInRound: number
+}
+
+type Action = {
+    setFlashcardSet: (flashcardSet: any) => void,
+    addToAllAnswers: (answer: any) => void,
+    addToRoundAnswers: (answer: any) => void,
+    resetRoundAnswers: () => void,
+    updateResult: (result: any) => void,
+    addToAllFlashcards: (flashcard: any) => void,
+    setRoundFlashcards: (flashcards: []) => void,
+    incrementCurrentFlashcardIndexInRound: () => void,
+    resetCurrentFlashcardIndexInRound: () => void,
+}
+
+const useTrainingStore = create<State & Action>((set) => ({
+    flashcardSet: {},
+    allAnswers: [],
+    roundAnswers: [],
+    result: null,
+    allFlashcards: [],
+    roundFlashcards: [],
+    currentFlashcardIndexInRound: 0,
+    setFlashcardSet: (flashcardSet) => set(() => ({ flashcardSet: flashcardSet })),
+    addToAllAnswers: (answer) => set((state) => ({ allAnswers: [...state.allAnswers, answer] })),
+    addToRoundAnswers: (answer) => set((state) => ({ roundAnswers: [...state.roundAnswers, answer] })),
+    resetRoundAnswers: () => set(() => ({ roundAnswers: [] })),
+    updateResult: (result) => set(() => ({ result: result })),
+    addToAllFlashcards: (flashcard) => set((state) => ({ allFlashcards: [...state.allFlashcards, flashcard] })),
+    setRoundFlashcards: (flashcards) => set(() => ({ roundFlashcards: flashcards })),
+    incrementCurrentFlashcardIndexInRound: () => set((state) => ({ currentFlashcardIndexInRound: state.currentFlashcardIndexInRound + 1 })),
+    resetCurrentFlashcardIndexInRound: () => set(() => ({ currentFlashcardIndexInRound: 0 })),
+}))
+
+export default useTrainingStore;
