685268fcfef3d5df60814ed74246c12cdbc057e6
Marek Czopor <marqos111222@gmail.com>
2023-12-17 00:31:50 +0100

    feature #92: connect main content to flashcardSet value

--- mystery-flashcards/app/[locale]/(flashcardsLearn)/flashcards/[id]/page.tsx
+++ mystery-flashcards/app/[locale]/(flashcardsLearn)/flashcards/[id]/page.tsx
@@ -1,6 +1,6 @@
 import FlashcardContainer from '@/components/common/FlashcardContainer';
 import { fetchDictionary } from '@/dictionaries/dictionaries';
-import { FlashcardT } from '@/models/Flashcard'
+import { FlashcardSetT } from '@/models/FlashcardSet';
 import { getFlashcardSetRequest } from '@/utils/client/ApiUtils';
 import { createCookieHeader } from '@/utils/client/RestUtils';
 import { HeartIcon } from "@heroicons/react/24/outline"
@@ -10,30 +10,20 @@ import React from 'react'
 export default async function FlashcardsDetails({ params }: { params: { locale: string, id: string } }) {
   const dictionary = await fetchDictionary(params.locale);
   const flashcardSetId = params.id;
-  const flashcardSet = await getFlashcardSetRequest(flashcardSetId, createCookieHeader(cookies()));
+  const { flashcardSet }: { flashcardSet: FlashcardSetT } = await getFlashcardSetRequest(flashcardSetId, createCookieHeader(cookies()));
 
-  const singleFlashcard: FlashcardT = {
-    wordLang1: "słowo",
-    wordLang2: "word",
-    description1: "Jakiś opis słowa",
-    description2: "Some description"
-  }
-
-  const flashcards: FlashcardT[] = Array(20).fill(singleFlashcard);
   return (
-    <>
-      <div className="w-[1000px]">
-        {flashcards.map(card => <FlashcardContainer dictionary={dictionary} key={card.wordLang1} card={card} />)}
-      </div>
-      <div>FlashcardsDetails {params.id}</div>
-    </>
+    <div className="w-[1000px]">
+      {renderActionButtons()}
+      {flashcardSet.flashcards?.map(card => <FlashcardContainer dictionary={dictionary} key={card.wordLang1} card={card} />)}
+    </div>
   )
 
   function renderActionButtons() {
     return (
       <div className="mb-12 flex justify-end">
-        <button className="btn btn-primary mr-10">Ucz się tej kolekcji</button>
-        <div className="tooltip tooltip-bottom" data-tip="Dodaj do ulubionych">
+        <button className="btn btn-primary mr-10">{dictionary.common.learnThisFlashcardSet}</button>
+        <div className="tooltip tooltip-bottom" data-tip={dictionary.common.addToFavorites}>
           <button className="btn btn-primary"> &nbsp;<HeartIcon className="h-6 w-6" /> &nbsp; </button>
         </div>
       </div>
