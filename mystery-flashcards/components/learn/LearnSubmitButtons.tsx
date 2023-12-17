d542a9186800adc9b794d21fcc0c5e72789e2e80
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-17 14:55:50 +0100

    feature #77: refactor - preparing for different submit buttons behaviours

--- mystery-flashcards/components/learn/LearnSubmitButtons.tsx
+++ mystery-flashcards/components/learn/LearnSubmitButtons.tsx
@@ -1,51 +1,14 @@
 "use client"
-import { AlertType } from '@/enums/AlertType';
-import useAlertStore from '@/stores/useAlertStore';
-import useTrainingStore from '@/stores/useTrainingStore';
-import { patchAnswersAndReturnResults } from '@/utils/client/ApiUtils';
-import { getMainButtonAttrs } from '@/utils/client/TrainingUtils';
-import { getNestedFieldByPath } from '@/utils/server/objectUtils';
-import { usePathname } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import React, { useState, useEffect, useRef } from 'react'
+import useSubmitLearnAction from '@/hooks/useSubmitLearnAction';
+import React from 'react'
 
 function LearnSubmitButtons({ dictionary }: { dictionary: any }) {
-    const wasChecked = useTrainingStore((state) => state.wasChecked);
-    const roundAnswers = useTrainingStore((state) => state.roundAnswers);
-    const setFinalResult = useTrainingStore((state) => state.setFinalResult);
-    const addAlert = useAlertStore((state) => state.add)
-    const router = useRouter();
-    const [kwasChecked, setKwasChecked] = useState<Boolean>(false);
-
-    const setWasRef = useRef<any>(null)
-    setWasRef.current = wasChecked;
-
-    useEffect(() => {
-        setKwasChecked(wasChecked)
-    }, [wasChecked])
-    const tempId = "656a2c5d573e1d09a12fd05a";
-    const mainButtonAttrs: ButtonAttrs = getMainButtonAttrs(setWasRef.current);
-    const pathname = usePathname();
-    const title = pathname.includes("result") ? dictionary.common.answersResultsTitle :
-        pathname.includes("training") ? dictionary.common.learnTitle : dictionary.common.testTitle;
-    const goToResults = () => router.push(`/learn/training/${tempId}/results`)
-    const onFinishClick = async () => {
-
-        try {
-            const result = await patchAnswersAndReturnResults(tempId, roundAnswers);
-            setFinalResult(result);
-            console.log("finalResult: ", result)
-            goToResults();
-        } catch (errorResponse: any) {
-            addAlert({ type: AlertType.error, title: getNestedFieldByPath(dictionary, errorResponse.body.message) })
-        }
-
-    }
+    const { mainButtonAttrs, otherButtonAttrs } = useSubmitLearnAction({ dictionary })
 
     return (
         <>
             <button className="btn btn-primary mb-3 btn-wide" {...mainButtonAttrs}>{mainButtonAttrs.title}</button>
-            <button className="btn btn-secondary mb-3 btn-wide" onClick={onFinishClick}>Zakończ naukę</button>
+            <button className="btn btn-secondary mb-3 btn-wide" onClick={otherButtonAttrs.onFinishClick}>{otherButtonAttrs.title}</button>
         </>
     )
 }
---
+++ mystery-flashcards/hooks/useSubmitLearnAction.ts
@@ -0,0 +1,36 @@
+import { AlertType } from "@/enums/AlertType";
+import useAlertStore from "@/stores/useAlertStore";
+import useTrainingStore from "@/stores/useTrainingStore";
+import { patchAnswersAndReturnResults } from "@/utils/client/ApiUtils";
+import { getMainButtonAttrs } from "@/utils/client/TrainingUtils";
+import { getNestedFieldByPath } from "@/utils/server/objectUtils";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+
+function useSubmitLearnAction({ dictionary }: { dictionary: any }) {
+    const wasChecked = useTrainingStore((state) => state.wasChecked);
+    const roundAnswers = useTrainingStore((state) => state.roundAnswers);
+    const setFinalResult = useTrainingStore((state) => state.setFinalResult);
+    const addAlert = useAlertStore((state) => state.add)
+    const router = useRouter();
+    const wasCheckedRef = useRef<any>(null)
+    wasCheckedRef.current = wasChecked;
+    const tempId = "656a2c5d573e1d09a12fd05a";
+    const mainButtonAttrs: ButtonAttrs = getMainButtonAttrs(wasCheckedRef.current);
+    const goToResults = () => router.push(`/learn/training/${tempId}/results`)
+    const onFinishClick = async () => {
+        try {
+            const result = await patchAnswersAndReturnResults(tempId, roundAnswers);
+            setFinalResult(result);
+            goToResults();
+        } catch (errorResponse: any) {
+            addAlert({ type: AlertType.error, title: getNestedFieldByPath(dictionary, errorResponse.body.message) })
+        }
+
+    }
+    const otherButtonAttrs = { onFinishClick, title: "Zakończ naukę" }
+
+    return { mainButtonAttrs, otherButtonAttrs };
+}
+
+export default useSubmitLearnAction;
