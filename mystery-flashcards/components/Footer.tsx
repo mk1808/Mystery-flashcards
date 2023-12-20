293c6054b800f346652431455a196d31d76a8249
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-20 23:01:24 +0100

    bug #108: create hook for checking menu items to display

--- mystery-flashcards/components/Footer.tsx
+++ mystery-flashcards/components/Footer.tsx
@@ -1,10 +1,71 @@
 "use client"
+import useLoggedUserForLayout from '@/hooks/useLoggedUserForLayout';
 import Link from 'next/link';
 import React from 'react'
 
-function Footer({ dictionary }: { dictionary: any }) {
+function Footer({ dictionary, locale }: { dictionary: any, locale: any }) {
     const year = new Date().getFullYear();
-    const cl = () => console.log("aaa")
+    const { renderMenuElementIfNeeded, onLogout } = useLoggedUserForLayout({ renderMenuElement });
+
+    const mainMenuElements = [
+        {
+            name: dictionary.common.mainPage,
+            link: `/${locale}`,
+            forAll: true
+        },
+        {
+            name: dictionary.common.searchSets,
+            link: `/${locale}#flashcardSetsSearch`,
+            forAll: true
+        },
+        {
+            name: dictionary.common.addNewSet,
+            link: `/${locale}/flashcards/new`,
+            forLogged: true
+        },
+        {
+            name: dictionary.common.mySets,
+            link: `/${locale}#flashcardSetsSearch`,
+            forLogged: true
+        },
+        {
+            name: dictionary.common.login,
+            link: `/${locale}/login`,
+            forNotLogged: true
+        },
+        {
+            name: dictionary.common.register,
+            link: `/${locale}/register`,
+            forNotLogged: true
+        },
+    ]
+
+    const languageMenuElements = [
+        {
+            name: dictionary.common.mainPage,
+            link: `/${locale}`,
+            forAll: true
+        },
+        {
+            name: dictionary.common.searchSets,
+            link: `/${locale}#flashcardSetsSearch`,
+            forAll: true
+        },
+    ]
+
+    const contactMenuElements = [
+        {
+            name: "GitHub - Monika Kordoń",
+            link: `https://github.com/mk1808`,
+            forAll: true
+        },
+        {
+            name: "GitHub - Marek Czopor",
+            link: `https://github.com/marqos12`,
+            forAll: true
+        },
+    ]
+
     return (
         <footer className="footer p-10 mt-4 bg-base-200 text-base-content grid-cols-5">
             <aside>
@@ -17,27 +78,34 @@ function Footer({ dictionary }: { dictionary: any }) {
             <nav />
             <nav className="ps-3 border-s-2 border-slate-600">
                 <header className="footer-title">{dictionary.common.browse}</header>
-                <a className="link link-hover">{dictionary.common.mainPage}</a>
-                <a className="link link-hover">{dictionary.common.searchSets}</a>
-                <a className="link link-hover">{dictionary.common.login}</a>
-                <a className="link link-hover">{dictionary.common.register}</a>
+                {mainMenuElements.map(renderMenuElementIfNeeded)}
+
             </nav>
             <nav className="ps-3 border-s-2 border-slate-600">
-                <header className="footer-title">Język</header>
+                <header className="footer-title">{dictionary.common.language}</header>
                 <a className="link link-hover">polski</a>
-                <a className="link link-hover" href='javascript:;' onClick={cl} >angielski</a>
+                <a className="link link-hover"  >angielski</a>
                 <a className='min-h-[1.25rem]'></a>
                 <a className='min-h-[1.25rem]'></a>
             </nav>
             <nav className="ps-3 border-s-2 border-slate-600">
-                <header className="footer-title">Kontakt</header>
-                <a className="link link-hover">GitHub - Monika Kordoń</a>
-                <a className="link link-hover">GitHub - Marek Czopor</a>
+                <header className="footer-title">{dictionary.common.contact}</header>
+                {contactMenuElements.map(renderMenuElementIfNeeded)}
                 <a className='min-h-[1.25rem]'></a>
                 <a className='min-h-[1.25rem]'></a>
             </nav>
         </footer>
     )
+
+
+    function renderMenuElement(element: any) {
+        console.log(element)
+        if (element.onClick) {
+            return <a className="link link-hover min-h-[1.25rem]" key={element.name} onClick={element.onClick}>{element.name}</a>
+
+        }
+        return <Link href={element.link}>{element.name}</Link>;
+    }
 }
 
 export default Footer
