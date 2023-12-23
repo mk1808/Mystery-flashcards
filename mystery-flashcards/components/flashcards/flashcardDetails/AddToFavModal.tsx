b27e9afd945265f8d14e41ca3bf4ca665e4ab5e5
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-23 19:11:33 +0100

    feature #147: display heart when in favorites

--- mystery-flashcards/components/flashcards/flashcardDetails/AddToFavModal.tsx
+++ mystery-flashcards/components/flashcards/flashcardDetails/AddToFavModal.tsx
@@ -1,21 +1,28 @@
 import Modal from '@/components/common/Modal'
 import React from 'react'
 import { HeartIcon } from "@heroicons/react/24/outline"
+import { HeartIcon as FullHeartIcon } from "@heroicons/react/24/solid"
 import AddToFavActions from './AddToFavActions'
 import AddToFavContent from './AddToFavContent'
 
 function AddToFavModal({ dictionary, flashcardSet }: { dictionary: any, flashcardSet: any }) {
+    const isFav = flashcardSet?.userFlashcard?.isFavorite;
     return <Modal modalTrigger={renderDialogTrigger()}
         dialogHeader={renderDialogHeader()}
         dialogContent={renderDialogContent()}
         dialogActions={<AddToFavActions dictionary={dictionary} flashcardSet={flashcardSet} />}
         width="w-5/12"
+        disabled={isFav}
     />
 
     function renderDialogTrigger() {
         return (
-            <div className="tooltip tooltip-bottom" data-tip={dictionary.common.addToFavorites}>
-                <button className="btn btn-primary"> &nbsp;<HeartIcon className="h-6 w-6" /> &nbsp; </button>
+            <div className="tooltip tooltip-bottom" data-tip={isFav?dictionary.common.alreadyInFavorites:dictionary.common.addToFavorites}>
+                <button className="btn btn-primary">
+                    &nbsp;
+                    {isFav == true ? <FullHeartIcon className="h-6 w-6" /> : <HeartIcon className="h-6 w-6" />}
+                    &nbsp;
+                </button>
             </div>
         )
     }
