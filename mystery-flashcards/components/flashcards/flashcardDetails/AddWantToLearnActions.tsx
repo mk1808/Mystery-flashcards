0d319a2b6fa02f2f0dbcebff1b1ad235b980c188
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-23 22:38:09 +0100

    feature #147: want to learn - continuation

--- mystery-flashcards/components/flashcards/flashcardDetails/AddWantToLearnActions.tsx
+++ mystery-flashcards/components/flashcards/flashcardDetails/AddWantToLearnActions.tsx
@@ -1,9 +1,35 @@
+"use client"
+import { AlertType } from '@/enums/AlertType';
+import useAlertStore from '@/stores/useAlertStore';
+import { postUserFlashcardSet } from '@/utils/client/ApiUtils';
+import { getNestedFieldByPath } from '@/utils/server/objectUtils';
 import React from 'react'
 
-function AddWantToLearnActions() {
-  return (
-    <div>AddWantToLearnActions</div>
-  )
+function AddWantToLearnActions({ dictionary, flashcardSet }: { dictionary: any, flashcardSet: any }) {
+    const addAlert = useAlertStore((state) => state.add)
+    const onSubmit = async () => { document.getElementById("addWantToLearn")!.innerHTML = ""
+      const body = {
+        flashcardSetId: flashcardSet._id,
+        type: "NONE",
+        isFavorite: true
+      }
+      try {
+       // const response = await postUserFlashcardSet(body);
+       
+      //  document.getElementById("addToFavTooltip")!.setAttribute("data-tip", dictionary.common.alreadyInFavorites);
+        
+        addAlert({ type: AlertType.success, title: dictionary.common.addToFavSuccess })
+      } catch (errorResponse: any) {
+        addAlert({ type: AlertType.error, title: getNestedFieldByPath(dictionary, errorResponse.body.message) })
+      }
+  
+    }
+    return (
+      <>
+        <button className="btn ml-3">{dictionary.common.close}</button>
+        <button className="btn btn-primary ml-3" onClick={onSubmit}> {dictionary.common.confirm}</button>
+      </>
+    )
 }
 
 export default AddWantToLearnActions
--- mystery-flashcards/components/flashcards/flashcardDetails/AddWantToLearnModal.tsx
+++ mystery-flashcards/components/flashcards/flashcardDetails/AddWantToLearnModal.tsx
@@ -1,9 +1,38 @@
+import Modal from '@/components/common/Modal';
 import React from 'react'
+import AddWantToLearnActions from './AddWantToLearnActions';
+import AddWantToLearnContent from './AddWantToLearnContent';
+import { FolderPlusIcon } from '@heroicons/react/24/outline';
+import { UserFlashcardT } from '@/models/UserFlashcard';
 
-function AddWantToLearnModal() {
-  return (
-    <div>AddWantToLearnModal</div>
-  )
+function AddWantToLearnModal({ dictionary, flashcardSet, userFlashcard }: { dictionary: any, flashcardSet: any, userFlashcard: UserFlashcardT }) {
+    const isFav = userFlashcard?.isFavorite;
+
+    return <Modal modalTrigger={renderDialogTrigger()}
+        dialogHeader={renderDialogHeader()}
+        dialogContent={renderDialogContent()}
+        dialogActions={<AddWantToLearnActions dictionary={dictionary} flashcardSet={flashcardSet} />}
+        width="w-5/12"
+
+    />
+
+    function renderDialogTrigger() {
+        return (
+            <div className="tooltip tooltip-bottom" id="addWantToLearn" data-tip={dictionary.common.wantToLearn}>
+                <button className="btn btn-primary px-10 ml-2">
+                    <FolderPlusIcon className="h-6 w-6" />
+                </button>
+            </div>
+        )
+    }
+
+    function renderDialogHeader() {
+        return <h2 className="font-bold text-3xl">{dictionary.common.wantToLearnTitle}</h2>
+    }
+
+    function renderDialogContent() {
+        return <AddWantToLearnContent dictionary={dictionary} flashcardSet={flashcardSet} />
+    }
 }
 
 export default AddWantToLearnModal
