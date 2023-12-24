08a83b2e78e874bdd93ddf077b6633fe0f09d56f
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-24 23:34:17 +0100

    feature #162: user details fix

--- mystery-flashcards/components/user/UserStatistics.tsx
+++ mystery-flashcards/components/user/UserStatistics.tsx
@@ -29,16 +29,24 @@ export default function UserStatistics({ dictionary }: { dictionary: any }) {
 
     function renderCell(title: string, value: any) {
         return (
-            <div className="grid grid-cols-1 text-center p-10 h-[250px] relative">
-                <div className="font-bold">{title}</div>
-                <div className="absolute bottom-20 w-full">{renderValueOrLoader(value)}</div>
+            <div className="grid grid-cols-1 text-center p-6 h-[250px] relative ">
+<div className="stats shadow">
+
+                    <div className="stat whitespace-normal">
+                        <div className="stat-title whitespace-normal">{title}</div>
+                        <div className="stat-value">{renderValueOrLoader(value)}</div>
+                    </div>
+
+                </div>
+            
+
             </div>
         );
     }
 
     function renderValueOrLoader(value: any) {
         if (value != undefined) {
-            return value;
+            return value.length>24?value:<>{value}<br/></> ;
         }
         return <span className="loading loading-ball loading-sm"></span>
     }
