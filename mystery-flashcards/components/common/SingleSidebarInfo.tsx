ad89785fd52bf229648b32b66372b6f907ceffbd
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-24 23:33:32 +0100

    feature #162: small fixes, change sidebar

--- mystery-flashcards/components/common/SingleSidebarInfo.tsx
+++ mystery-flashcards/components/common/SingleSidebarInfo.tsx
@@ -5,10 +5,10 @@ function SingleSidebarInfo({ title, value }: { title: any, value: any }) {
 
     return (
         <div className="my-3 flex items-center">
-            <ChevronDoubleRightIcon className="h-5 w-5 mr-2 text-gray-500" />
-            <span className="text-xl">{title}</span>
-            <span className="text-xl">: &nbsp;</span>
-            <span className="text-xl">{value}</span>
+            <ChevronDoubleRightIcon className="h-5 w-6 mr-2 text-gray-800" />
+            <span className="text-xl font-light">{title}</span>
+            <span className="text-xl font-light">: &nbsp;</span>
+            <span className="text-xl font-medium">{value}</span>
         </div>
     )
 }
