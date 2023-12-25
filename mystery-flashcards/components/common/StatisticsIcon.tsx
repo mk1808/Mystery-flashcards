ad71bca474cfc7a57fac4c46e0d4d7117b7ecae4
Monika Kordoń <37344587+mk1808@users.noreply.github.com>
2023-12-25 14:06:37 +0100

    feature #162: fix sidebars

--- mystery-flashcards/components/common/StatisticsIcon.tsx
+++ mystery-flashcards/components/common/StatisticsIcon.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function StatisticsIcon() {
     return (
         <div className='flex justify-center'>
-          <div className='py-[4px] px-[8px] bg-base-100 border border-base-100 rounded '>
+          <div className='py-[4px] px-[12px] bg-base-100 border border-base-100 rounded '>
             <ChartBarIcon className="h-8 w-8 text-primary" />
           </div>
         </div>
